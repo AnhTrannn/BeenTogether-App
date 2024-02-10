5fff1e1674d16e60d2f855a8e56df9af590dbd4a
Ace <qanhltv11@gmail.com>
2024-02-10 16:36:14 +0700

    Fix time error

--- main.js
+++ main.js
@@ -1,42 +1,25 @@
-// const timeEl = document.getElementById('time');
-// setInterval(() => {
-//     const now = new Date();
-//     timeEl.innerHTML = `Time: ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-// }, 1000);
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     let days = document.querySelector('.days span');
-// 	let hour = document.querySelector('.hours');
-// 	let min = document.querySelector('.minutes');
-// 	let second = document.querySelector('.seconds');
-// 	let startDate = new Date(2020, 01, 25);
-// 	days.innerText = Math.floor((new Date - startDate) / 86400000);
-// 	countTime();
-
-// 	function countTime() {
-// 		let today = new Date();
-// 		let ms = (today - startDate) % 86400000;
-// 		hour.innerText = Math.floor(ms / 3600000);
-// 		min.innerText = Math.floor(ms % 3600000 / 60000);
-// 		second.innerText = Math.floor(ms % 3600000 % 60000 / 1000);
-// 	}
-// 	setInterval(countTime, 1000);
-// }, false);
-
 let days = document.querySelector('.days span');
-let hour = document.querySelector('.hours');
-let min = document.querySelector('.minutes');
-let second = document.querySelector('.seconds');
-let startDate = new Date(2020, 1, 25);
-days.innerText = Math.floor((new Date - startDate) / 86400000);
+let hours = document.querySelector('.hours');
+let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('.seconds');
+let startDate = new Date(2020,1,25);
 
 updateTime();
-setInterval(updateTime, 1000);
+setInterval(updateTime,1000);
 
 function updateTime() {
-    let today = new Date();
-	let ms = (today - startDate) % 86400000;
-	hour.innerText = Math.floor(ms / 3600000);
-	min.innerText = Math.floor(ms % 3600000 / 60000);
-	second.innerText = Math.floor(ms % 3600000 % 60000 / 1000);
+	let today = new Date();
+	let timeDiff = today - startDate;
+
+	// Calculate days, hours, minutes, and seconds
+	let daysCount = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    let hoursCount = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutesCount = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    let secondsCount = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    // Update the HTML elements
+    days.innerText = daysCount;
+    hours.innerText = hoursCount;
+    minutes.innerText = minutesCount;
+    seconds.innerText = secondsCount;
 }
