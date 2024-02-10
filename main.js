48b9def016c0fe63ce8b10d11d34789d33ae92fc
Ace <qanhltv11@gmail.com>
2024-02-10 16:52:02 +0700

    Finally fix time error

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
 let days = document.querySelector('.days span');
 let hours = document.querySelector('.hours');
-let minutes = document.querySelector('.minutes');
-let seconds = document.querySelector('.seconds');
-let startDate = new Date(2020,1,25);
+let min = document.querySelector('.minutes');
+let second = document.querySelector('.seconds');
+
+let startDate = new Date(2020, 0, 25);
 
 updateTime();
-setInterval(updateTime,1000);
+setInterval(updateTime, 1000);
 
 function updateTime() {
-	let today = new Date();
-	let timeDiff = today - startDate;
+    let today = new Date();
+    let timeDifference = today - startDate;
 
-	// Calculate days, hours, minutes, and seconds
-	let daysCount = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    let hoursCount = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutesCount = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    let secondsCount = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    // Calculate total seconds
+    let totalSeconds = Math.floor(timeDifference / 1000);
+
+    // Calculate days, hours, minutes, and seconds
+    let daysCount = Math.floor(totalSeconds / (24 * 60 * 60));
+    let hoursCount = Math.floor((totalSeconds % (24 * 60 * 60)) / 3600);
+    let minutesCount = Math.floor((totalSeconds % 3600) / 60);
+    let secondsCount = totalSeconds % 60;
 
     // Update the HTML elements
     days.innerText = daysCount;
     hours.innerText = hoursCount;
-    minutes.innerText = minutesCount;
-    seconds.innerText = secondsCount;
+    min.innerText = minutesCount;
+    second.innerText = secondsCount;
 }
